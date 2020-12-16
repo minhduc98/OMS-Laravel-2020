13bb4f18bb07059fcec556805a9d94c606c46c37
Đăng Minh Đức Đỗ <minhduc100298@gmail.com>
2020-12-16 19:47:56 +0700

    gasdgsdgadsag

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,9 @@
 |
 */
 
-header('Access-Control-Allow-Origin: *'); header('Access-Control-Allow-Methods: *'); header('Access-Control-Allow-Headers: Origin, X-Requested-With,Authorization,  Content-Type, Accept');
+header('Access-Control-Allow-Origin', '*');
+header('Access-Control-Allow-Methods', 'GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS');
+header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
 $app = new Illuminate\Foundation\Application(
     $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
