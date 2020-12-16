2cb30ed4ff06323e886e6edafa6b38de0c8627f7
Đăng Minh Đức Đỗ <minhduc100298@gmail.com>
2020-12-16 19:43:51 +0700

    GG

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -22,6 +22,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\Cors::class,
+        \Fruitcake\Cors\HandleCors::class,
     ];
 
     /**
@@ -43,6 +44,7 @@ class Kernel extends HttpKernel
         'api' => [
             'throttle:60,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \Fruitcake\Cors\HandleCors::class,
         ],
     ];
 
